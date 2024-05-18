4704c6fb6d7976103ce789c2db3c200bfb8b0c75
PHaasbroek <phaasbroek97@gmail.com>
2024-05-18 19:49:54 +0200

    print board feature incomplete

---
+++ Header.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+class My_Class
+{
+public:
+	My_Class() {
+		
+		std::vector<char> row;
+
+		for (int i = 0; i < 8; i++) {
+			row.push_back(' ');
+		}
+
+		for (int j = 0; j < 8; j++) {
+			board.push_back(row);
+		}
+		
+		std::cout << cell << std::endl;
+
+		std::cout << "Chess board initiated. ";
+		std::cout << std::endl;
+	}
+
+	void print_board() {
+
+		for (int i = 0; i < 8; i++) {
+
+			for (int j = 0; j < 8; j++) {
+
+				if (i == 0 || j == 0) {
+					std::cout << "+";
+				}
+				
+				std::cout << " | ";
+
+
+
+				std::cout << board[i][j];
+				std::cout << " | ";
+			}
+			std::cout << "\n";
+		}
+		std::cout << std::endl;
+	}
+
+private:
+	//std::vector<char> row;
+	std::vector < std::vector <char> > board;
+	char cell = ' ';
+};
--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+#include "Header.h"
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    
+    My_Class chess;
+    
+    chess.print_board();
+
     return 0;
 }
+
+
